3bb672d59a0fcbcce4cfc3c0172cef33e1998b9b
Tom Wagg <tomjwagg@gmail.com>
2026-06-15 15:30:46 -0400

    consts no longer needed

--- src/backpop/__init__.py
+++ src/backpop/__init__.py
@@ -1,4 +1,4 @@
-from . import main, posteriors, phase, consts, files
+from . import main, posteriors, phase, files
 from ._version import __version__
 from .main import BackPop
 from .posteriors import BackPopsteriors
